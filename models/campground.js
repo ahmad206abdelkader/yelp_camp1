87753c225529ae30d7aafe6a290f6801fa2a9b63
unknown <ahmad11iphone11abdelkader11@gmail.com>
2022-05-20 02:10:08 +0300

    first commit

---
+++ models/campground.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const CampgroundSchema = new Schema({
+    thtle: String,
+    price: String,
+    description: String,
+    location: String
+});
+
+
+module.exports = mongoose.model('Campground', CampgroundSchema);
