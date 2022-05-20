5ff98634e758a89d77dd3bd273b8fe4e1f613d92
unknown <ahmad11iphone11abdelkader11@gmail.com>
2022-05-20 22:53:01 +0300

    first commit

---
+++ seeds/seedHelpers.js
@@ -0,0 +1,47 @@
+module.exports.descriptors = [
+    'Forest',
+    'Ancient',
+    'Petrified',
+    'Roaring',
+    'Cascade',
+    'Tumbling',
+    'Silent',
+    'Redwood',
+    'Bullfrog',
+    'Maple',
+    'Misty',
+    'Elk',
+    'Grizzly',
+    'Ocean',
+    'Sea',
+    'Sky',
+    'Dusty',
+    'Diamond'
+]
+
+
+
+module.exports.places = [
+    'Flats',
+    'viallage',
+    'Canyon',
+    'Pond',
+    'Group Camp',
+    'Horse Camp',
+    'Ghost Town',
+    'Camp',
+    'Dispersed Camp',
+    'Backcountry',
+    'Rever',
+    'Creek',
+    'Creekside',
+    'Bay',
+    'Spring',
+    'Bayshore',
+    'Sands',
+    'Mule camp',
+    'Hounting Camp',
+    'Cliffs',
+    'Hollow'
+
+]
